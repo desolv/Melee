c3172c200d0a8e195f6d6f66d156d23103fbe694
Michele <michele47801@gmail.com>
2024-09-20 08:03:42 +0100

    Add Profile class to handle players

---
+++ src/main/java/gg/desolve/melee/player/profile/Profile.java
@@ -0,0 +1,51 @@
+package gg.desolve.melee.player.profile;
+
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+public class Profile {
+
+    @Getter
+    private static Map<UUID, Profile> profiles = new HashMap<>();
+
+    private final UUID uuid;
+    private String username;
+    private int logins;
+    private Long firstSeen;
+    private Long lastSeen;
+    private String address;
+    private boolean loaded;
+
+    public Profile(UUID uuid, String username) {
+        this.uuid = uuid;
+        this.username = username;
+    }
+
+    public static Profile getProfile(UUID uuid) {
+        return profiles.containsKey(uuid) ?
+                profiles.get(uuid) :
+                new Profile(uuid, null);
+    }
+
+    public static Profile getProfile(String username) {
+        Profile profile = Optional.ofNullable(Bukkit.getPlayer(username))
+                .map(player -> profiles.get(player.getUniqueId()))
+                .orElse(null);
+
+        if (profile == null) {
+            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(username);
+            profile = offlinePlayer.hasPlayedBefore() && profiles.containsKey(offlinePlayer.getUniqueId())
+                    ? profiles.get(offlinePlayer.getUniqueId())
+                    : new Profile(offlinePlayer.getUniqueId(), offlinePlayer.getName());
+        }
+
+        return profile;
+    }
+}
