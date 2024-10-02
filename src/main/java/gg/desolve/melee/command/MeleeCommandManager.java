fe1de74e21dd19c2ff9e85aa620906c94591f39a
Michele <michele47801@gmail.com>
2024-10-02 08:08:25 +0100

    Add command completions for ranks, durations, and reasons

--- src/main/java/gg/desolve/melee/command/MeleeCommandManager.java
+++ src/main/java/gg/desolve/melee/command/MeleeCommandManager.java
@@ -4,8 +4,11 @@ import co.aikar.commands.CommandIssuer;
 import co.aikar.commands.InvalidCommandArgument;
 import co.aikar.commands.Locales;
 import co.aikar.commands.PaperCommandManager;
+import com.google.common.collect.ImmutableList;
 import gg.desolve.melee.Melee;
+import gg.desolve.melee.common.Duration;
 import gg.desolve.melee.player.profile.Profile;
+import gg.desolve.melee.rank.Rank;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
@@ -22,6 +25,7 @@ public class MeleeCommandManager extends PaperCommandManager {
         try {
             loadCommands();
             loadContexts();
+            loadCompletions();
 
             this.enableUnstableAPI("help");
             this.getLocales().loadYamlLanguageFile("messages.yml", Locales.ENGLISH);
@@ -45,6 +49,26 @@ public class MeleeCommandManager extends PaperCommandManager {
                     return Optional.ofNullable(profile).orElseThrow(() ->
                             new InvalidCommandArgument("&cPlayer matching &e" + popName + " &ccould not be found.", false));
                 });
+
+        getCommandContexts().registerContext(
+                Duration.class, c -> {
+                    String popDuration = c.popFirstArg();
+                    Duration duration = Duration.fromString(popDuration);
+                    return Optional.ofNullable(duration).orElseThrow(() ->
+                            new InvalidCommandArgument("&cDuration matching &e" + popDuration + " &ccould not be found.", false));
+                });
+    }
+
+    private void loadCompletions() {
+        getCommandCompletions().registerAsyncCompletion(
+                "ranks", c -> Rank.getRanks().keySet()
+        );
+
+        getCommandCompletions().registerAsyncCompletion("durations", c ->
+                ImmutableList.of("permanent", "s", "m", "h", "d", "w", "M", "y"));
+
+        getCommandCompletions().registerAsyncCompletion("reasons", c ->
+                ImmutableList.of("Store", "Whitelist", "Won", "Promoted", "Demoted", "Famous", "Other"));
     }
 
     @Override
