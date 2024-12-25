9c02782fd83b8ca936b6a91ba0e38960be8295ac
Michele <michele47801@gmail.com>
2024-12-25 16:15:24 +0000

    Add help and reset commands to Playtime

--- src/main/java/gg/desolve/melee/command/common/PlaytimeCommand.java
+++ src/main/java/gg/desolve/melee/command/common/PlaytimeCommand.java
@@ -1,6 +1,7 @@
 package gg.desolve.melee.command.common;
 
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.*;
 import gg.desolve.melee.command.inventory.grant.GrantInventory;
 import gg.desolve.melee.common.Converter;
@@ -9,12 +10,22 @@ import gg.desolve.melee.player.profile.Hunter;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+@CommandAlias("playtime")
 public class PlaytimeCommand extends BaseCommand {
 
+    @HelpCommand
+    @CommandPermission("melee.command.playtime|melee.command.playtime.help")
+    @Syntax("[page]")
+    @Description("Shows list of commands")
+    public static void onHelp(CommandHelp help) {
+        help.setPerPage(6);
+        help.showHelp();
+    }
+
     @CommandAlias("playtime")
-    @CommandPermission("melee.command.playtime")
+    @CommandPermission("melee.command.playtime|melee.command.playtime.playtime")
     @Description("Check your playtime")
-    public static void execute(Player player) {
+    public static void onPlaytime(Player player) {
         Hunter hunter = Hunter.getHunter(player.getUniqueId());
 
         Message.send(player,
@@ -26,10 +37,10 @@ public class PlaytimeCommand extends BaseCommand {
 
     @CommandAlias("playtime")
     @CommandCompletion("@players")
-    @CommandPermission("melee.command.playtime.others")
+    @CommandPermission("melee.command.playtime|melee.command.playtime.others")
     @Syntax("<player>")
     @Description("Check a player playtime")
-    public static void execute(CommandSender sender, Hunter hunter) {
+    public static void onPlaytime(CommandSender sender, Hunter hunter) {
         Message.send(sender,
                 "player% <green>total playtime is <yellow>playtime% <green>with <light_purple>logins% logins."
                         .replace("player%", hunter.getUsernameColored())
@@ -37,6 +48,21 @@ public class PlaytimeCommand extends BaseCommand {
                         .replace("logins%", String.valueOf(hunter.getLogins()))
         );
 
+    }
+
+    @Subcommand("reset")
+    @CommandCompletion("@players")
+    @CommandPermission("melee.command.playtime|melee.command.reset")
+    @Syntax("<player>")
+    @Description("Reset a player playtime")
+    public static void onReset(CommandSender sender, Hunter hunter) {
+        Message.send(sender,
+                "<green>You've reset player% playtime from <yellow>playtime% to 0 seconds."
+                        .replace("player%", hunter.getUsernameColored())
+                        .replace("playtime%", Converter.millisToTime(hunter.getPlaytime()))
+        );
+
+        hunter.setPlaytime(0L);
     }
 
 }
