c23da690fbc73991b9339fde889aa0f3eb117082
Michele <michele47801@gmail.com>
2025-03-07 19:11:07 +0000

    User sub command for profile retrieval

--- src/main/java/gg/desolve/melee/command/CommandDirector.java
+++ src/main/java/gg/desolve/melee/command/CommandDirector.java
@@ -6,6 +6,7 @@ import gg.desolve.melee.Melee;
 import gg.desolve.melee.command.management.GrantManualCommand;
 import gg.desolve.melee.command.management.InvalidateGrantCommand;
 import gg.desolve.melee.command.management.RankCommand;
+import gg.desolve.melee.command.management.UserCommand;
 import gg.desolve.melee.profile.Profile;
 import gg.desolve.melee.rank.Rank;
 import gg.desolve.mithril.Mithril;
@@ -40,7 +41,8 @@ public class CommandDirector {
         List<BaseCommand> commandList = Arrays.asList(
                 new GrantManualCommand(),
                 new InvalidateGrantCommand(),
-                new RankCommand()
+                new RankCommand(),
+                new UserCommand()
         );
 
         commandList.forEach(commandManager::registerCommand);
---
+++ src/main/java/gg/desolve/melee/command/management/UserCommand.java
@@ -0,0 +1,49 @@
+package gg.desolve.melee.command.management;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.CommandHelp;
+import co.aikar.commands.annotation.*;
+import gg.desolve.melee.Melee;
+import gg.desolve.melee.grant.GrantType;
+import gg.desolve.melee.inventory.rank.metadata.MetadataInventory;
+import gg.desolve.melee.inventory.rank.modify.ModifyInventory;
+import gg.desolve.melee.profile.Profile;
+import gg.desolve.melee.rank.Rank;
+import gg.desolve.melee.rank.RankManager;
+import gg.desolve.mithril.Mithril;
+import gg.desolve.mithril.relevance.Converter;
+import gg.desolve.mithril.relevance.Message;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandAlias("user")
+public class UserCommand extends BaseCommand {
+
+    @HelpCommand
+    @CommandPermission("melee.command.user|melee.command.user.help")
+    @Syntax("[page]")
+    public static void onHelp(CommandHelp help) {
+        help.setPerPage(6);
+        help.showHelp();
+    }
+
+    @Subcommand("simple")
+    @CommandCompletion("@players")
+    @CommandPermission("melee.command.user|melee.command.user.simple")
+    @Syntax("(player)")
+    @Description("Retrieve user information")
+    public static void onSimple(CommandSender sender, Profile profile) {
+        Message.send(sender,
+                "<newline>" + profile.getUsernameColored() + "<bold><aqua>'s User information</bold>" +
+                        "<newline><white>UUID: <dark_gray>" + profile.getUuid() +
+                        "<newline><white>Logins: <aqua>" + profile.getLogins() +
+                        "<newline><white>First Seen: <aqua>" + Converter.date(profile.getFirstSeen()) +
+                        "<newline><white>Last Seen: <aqua>" + Converter.time(System.currentTimeMillis() - profile.getLastSeen()) + "ago" +
+                        "<newline><white>Grants: <aqua>" + String.join("<white>, <white>", profile.getGrants().stream()
+                                .filter(grant -> grant.getType() == GrantType.ACTIVE)
+                                .map(grant -> (!grant.isPermanent() ? "<white>*" : "") + grant.getRank().getNameColored()).toList()) +
+                        "<newline><white>Sockets: <aqua>" + profile.getSockets().size());
+    }
+
+
+}
